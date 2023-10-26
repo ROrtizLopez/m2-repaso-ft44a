baf08356580cb7c10b522c3cf91a1ecd72ff846b
Raquel Ortiz Lopez <rortiz2401@gmail.com>
2023-10-26 07:04:16 -0600

    Routing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar(props) {
   return (
     <div>
-      <h2>NavBar</h2>
+      <span> Rick & Morty Episodes</span>
+
+      <button>
+        <NavLink
+          to='/home'
+          style={({ isActive }) => isActive ? {color: 'coral'} : {color: 'gray'}}
+        >
+          Home </NavLink>
+      </button>
+
+      <button>
+        <NavLink
+          to='/favorites'
+          style={({ isActive }) => isActive ? {color: 'coral'} : {color: 'gray'}}
+        >
+          Favorites</NavLink>
+      </button>
     </div>
   );
 }
