92cfbe3ca6a35ef970134d1a1691ee7d65681ec0
Raquel Ortiz Lopez <rortiz2401@gmail.com>
2023-10-26 08:30:45 -0600

    Redux

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import axios from 'axios';
+import Card from './Card';
+
 
 export default function Home(props) {
+
+  const [ episodes, setEpisodes ] = React.useState([]);
+
+  React.useEffect(() => {
+    axios('https://rickandmortyapi.com/api/episode')
+    .then(response => setEpisodes(response.data))
+    .catch(error => console.log(error))
+  }, [])
+
   return (
     <div>
-      <h2>Home</h2>
+      <h2> Episodes </h2>
+      <hr />
+      {episodes.results?.map((episode, index) => {
+        return(
+          <Card 
+            key={index}
+            episode={episode}
+          />
+        )
+        })
+      }
     </div>
   );
 }
